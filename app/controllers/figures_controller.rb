13bda9631c3c9a057957eec298cd9b64d1cd40f5
Tayari Appiah <cjhhh.ta@gmail.com>
2017-01-15 04:18:01 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,6 +1,7 @@
 class FiguresController < ApplicationController
 
   get '/figures' do
+    @figures = Figure.all
     erb :'/figures/index'
   end
 
@@ -13,9 +14,10 @@ class FiguresController < ApplicationController
     erb :'/figures/show'
   end
 
-  # get '/figures/:name/edit' do
-  #   erb :'/figures/edit'
-  # end
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
 
   post '/figures' do
     @figure = Figure.create(params["figure"])
@@ -27,6 +29,10 @@ class FiguresController < ApplicationController
       @figure.titles << Title.create(params[:title])
     end
     @figure.save
+  end
+
+  patch '/figures' do
+    @figure = Figure.find(params[:id])
   end
 
 end
