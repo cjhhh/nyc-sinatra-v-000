0a23a6879515db33b4fce19bcc76ce36f353a8d4
Tayari Appiah <cjhhh.ta@gmail.com>
2017-01-20 10:27:15 +0000

    field, new_landmark error

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -31,8 +31,19 @@ class FiguresController < ApplicationController
     @figure.save
   end
 
-  patch '/figures' do
+  post '/figures/:id' do
     @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+
+    # if !params[:landmark][:name].empty?
+    #   @figure.landmarks << Landmark.create(params[:landmark])
+    # end
+    # if !params[:title][:name].empty?
+    #   @figure.titles << Title.create(params[:title])
+    # end
+    # @figure.save
+
+    redirect to "/figures/#{@figure.id}"
   end
 
 end
